2114ed744ab57a028b3f1f32c369b63ac8019caa
이아현 <lah1203@naver.com>
2022-10-20 00:17:42 +0900

    fix: 마크다운 파싱 오류

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -3,15 +3,11 @@ import { Post } from '@/types/data';
 
 const parsePost = (id: Post['id'], post: string) => {
   const [, description, ...postContent] = post.split('<hr>');
-  const [, title, date] = description.replaceAll('<hr>', '').split('\n');
-
-  console.log('post: ', post);
-  console.log(description, postContent);
-  console.log(title, date);
+  const [title, date] = description.split('date: ');
 
   return {
     id,
-    title: title.replace('<p>title: ', ''),
+    title: title.split('<p>title: ')[1],
     date: date.replace('date: ', '').replace('</p>', ''),
     content: postContent.join('<hr>'),
   };
