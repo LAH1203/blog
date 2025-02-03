490185fa655c2c8b212de2d8755177b7e5be06ee
AhhyunLee <lah1203@naver.com>
2025-02-03 14:29:55 +0900

    사이드바 너비 이상하게 나오는 거 수정

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import Profile from './Profile';
 const Sidebar = () => {
   return (
     <aside className="sticky left-0 top-0 h-full w-[194px] xs:flex hidden items-center">
-      <div className="flex flex-col items-center justify-end gap-12 h-[90%] rounded-xl bg-[#B2C9AD] p-4">
+      <div className="flex flex-col items-center justify-end gap-12 h-[90%] rounded-xl bg-[#B2C9AD] p-4 w-full">
         <Profile />
         <Footer />
       </div>
