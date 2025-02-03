1f3ffdbfd1bce3e1d6a3ba8217f77bf0873c0c57
AhhyunLee <lah1203@naver.com>
2025-02-03 14:00:14 +0900

    모바일 메인에 푸터 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import PostItem from '@/components/PostItem/PostItem';
+import Footer from '@/components/Sidebar/Footer';
 import readAllPostsMetadata from '@/utils/readAllPostsMetadata';
 
 const Home = () => {
   const posts = readAllPostsMetadata();
 
   return (
-    <div className="h-full">
-      <ul className="grid xl-large:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-x-2 gap-y-4 last:pb-12 max-xs:first:pt-4 xs:w-fit w-full">
+    <div className="flex flex-col max-xs:items-end gap-8 h-full max-xs:pt-8">
+      <ul className="grid xl-large:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-x-2 gap-y-4 xs:pb-12 xs:w-fit w-full">
         {posts.map((post, idx) => (
           <PostItem item={post} key={idx} />
         ))}
       </ul>
+      <div className="xs:hidden block pb-8">
+        <Footer />
+      </div>
     </div>
   );
 };
