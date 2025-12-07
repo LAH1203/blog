dd8b3db397cda899a821a6d8a5063596ba1e1652
AhhyunLee <lah1203@naver.com>
2025-12-08 08:55:18 +0900

    일부 패키지 업데이트~~

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    });
-    return config;
-  },
   images: {
     remotePatterns: [
       {
